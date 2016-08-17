4d83bf57077820e798b689a728127b2c39617a1a
fishmov <yxwgle@gmail.com>
2016-08-17 20:04:19 +0800

    signal

---
+++ src/_20signals/signal_functions.h
@@ -0,0 +1,12 @@
+#ifndef SIGNAL_FUNCTIONS_H
+#define SIGNAL_FUNCTIONS_H
+
+#include <signal.h>
+#include "tlpi_hdr.h"
+
+int printSigMask(FILE *,const char *);
+int printPendingSigs(FILE *,const char *);
+
+void printSigset(FILE *,const char *,const sigset_t *mask);
+
+#endif
